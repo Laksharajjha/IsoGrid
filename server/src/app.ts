3452bdce5213ca5c7c6d26a0d364e98b6ca588a1
laksharajjha <laksharajjha@gmail.com>
2025-12-10 17:10:47 +0530

    feat(server): register bed routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import wardRoutes from './routes/wardRoutes';
+import bedRoutes from './routes/bedRoutes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/wards', wardRoutes);
+app.use('/api/beds', bedRoutes);
 
 export default app;
