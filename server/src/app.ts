6978351bcfa1adac60affe71c770293f0e8df18a
laksharajjha <laksharajjha@gmail.com>
2025-12-10 17:12:06 +0530

    feat(server): register booking routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import wardRoutes from './routes/wardRoutes';
 import bedRoutes from './routes/bedRoutes';
 import patientRoutes from './routes/patientRoutes';
+import bookingRoutes from './routes/bookingRoutes';
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use('/api/wards', wardRoutes);
 app.use('/api/beds', bedRoutes);
 app.use('/api/patients', patientRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 export default app;
