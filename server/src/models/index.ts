083c83e116e7757484aa8f2e88c000e0d5a4d70f
laksharajjha <laksharajjha@gmail.com>
2025-12-10 17:08:28 +0530

    feat(server): add models index and associations

---
+++ server/src/models/index.ts
@@ -0,0 +1,29 @@
+import Ward from './Ward';
+import Bed from './Bed';
+import Patient from './Patient';
+import Booking from './Booking';
+
+// Associations
+
+// Ward <-> Bed
+Ward.hasMany(Bed, { foreignKey: 'wardId', as: 'beds' });
+Bed.belongsTo(Ward, { foreignKey: 'wardId', as: 'ward' });
+
+// Bed <-> Patient
+Bed.hasOne(Patient, { foreignKey: 'bedId', as: 'currentPatient' });
+Patient.belongsTo(Bed, { foreignKey: 'bedId', as: 'bed' });
+
+// Patient <-> Booking
+Patient.hasMany(Booking, { foreignKey: 'patientId', as: 'bookings' });
+Booking.belongsTo(Patient, { foreignKey: 'patientId', as: 'patient' });
+
+// Bed <-> Booking
+Bed.hasMany(Booking, { foreignKey: 'bedId', as: 'bookings' });
+Booking.belongsTo(Bed, { foreignKey: 'bedId', as: 'bed' });
+
+export {
+    Ward,
+    Bed,
+    Patient,
+    Booking,
+};
